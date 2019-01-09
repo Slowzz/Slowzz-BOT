325b7ef6824288c1853f225d99fef4e39b2ce618
Slowzz <46527389+Slowzz@users.noreply.github.com>
2019-01-09 14:51:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cfg require('./index.json'); // a garder en version desktop
 const token = process.env.token // a garder en version heroku
 const prefix = ("?");
 
+
+bot.login(token);
+
+
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('rien').catch(console.error)
